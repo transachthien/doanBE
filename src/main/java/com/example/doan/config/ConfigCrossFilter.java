bc863f7ab3ff7b8ca869a6653e707a8c8ff17bf6
transachthien <thientshe141579@fpt.edu.vn>
2022-12-01 15:41:45 +0700

    commit cross

--- src/main/java/com/example/doan/config/ConfigCrossFilter.java
+++ src/main/java/com/example/doan/config/ConfigCrossFilter.java
@@ -12,7 +12,7 @@ public class ConfigCrossFilter {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://146.190.86.26:4200"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
