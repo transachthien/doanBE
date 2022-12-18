ff3b4cd2219941665baa78e3c78c51a2fb9cefd5
transachthien <thientshe141579@fpt.edu.vn>
2022-12-18 15:35:00 +0700

    commit Role

---
+++ src/main/java/com/example/doan/model/Role.java
@@ -0,0 +1,13 @@
+package com.example.doan.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    public String name;
+}
